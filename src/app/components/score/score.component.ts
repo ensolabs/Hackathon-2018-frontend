0a7bc61a5eb28f33e5cc621620687507e47f8c97
Trude <trude.martinsen@gmail.com>
2018-04-13 14:25:36 +0200

    Added list with scores

--- src/app/components/score/score.component.ts
+++ src/app/components/score/score.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../../services/backend.service';
+import { ScoreEntry } from  '../../model/all';
 
 @Component({
   selector: 'score',
@@ -8,10 +9,14 @@ import { BackendService } from '../../services/backend.service';
 })
 export class ScoreComponent implements OnInit {
 
+  public scores: ScoreEntry[] = [new ScoreEntry(1, 'bygg', 'build', 10), new ScoreEntry(2, 'ro ro', 'rowing', 20)];
+
   constructor(public _service:BackendService) { }
 
   ngOnInit() {
-    this._service.scoreStuff().subscribe(x=>{console.log(x)},err=>{console.log(err)});
+    //this._service.scoreStuff().subscribe(x=>{console.log(x)},err=>{console.log(err)});
   }
+
+
 
 }
---
+++ src/app/model/scoreEntry.ts
@@ -0,0 +1,5 @@
+export class ScoreEntry {
+  constructor(public id:number, public title:string, public image:string, public score:number){
+
+  }
+}
