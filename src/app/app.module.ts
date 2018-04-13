31cb7a905a1ee45e5632b3f016bba960f412a223
Snorre Garmann <snorre@outlook.com>
2018-04-13 11:15:48 +0200

    la til angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule,MatFormFieldModule,MatInputModule, MatOptionModule, MatSelectModule, MatSlideToggleModule, MatCardModule, MatChipsModule,
+  MatToolbarModule,  MatListModule, MatIconModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatFormFieldModule,MatInputModule,
+                MatOptionModule, MatSelectModule,MatSlideToggleModule,MatCardModule,
+                MatChipsModule, MatToolbarModule, MatIconModule,MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
