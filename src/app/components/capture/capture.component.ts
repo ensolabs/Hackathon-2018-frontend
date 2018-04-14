502dfdd2acef2759e8bcdb4c9bd6c47506359ae2
Trude <trude.martinsen@gmail.com>
2018-04-14 12:18:44 +0200

    Lagt til forhåndsvisning av bilde

--- src/app/components/capture/capture.component.ts
+++ src/app/components/capture/capture.component.ts
@@ -10,7 +10,8 @@ import { BackendService } from '../../services/backend.service';
 export class CaptureComponent implements OnInit {
 
   webcam:WebCamComponent//will be populated by <ack-webcam [(ref)]="webcam">
-  base64
+  base64;
+  public showCapture:boolean = false;
 
   options:any = {
     video:true,
@@ -28,7 +29,33 @@ export class CaptureComponent implements OnInit {
   onCamSuccess(){}
 
   captureImage() {
-    this.webcam.getBase64()
+
+    const video = <any>document.getElementsByTagName('video')[0];
+    const canvas = <any>document.getElementsByTagName('canvas')[0];
+    if (video) {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0);
+    }
+
+    console.log('show capture');
+    this.showCapture = true;
+
+  }
+
+  submitImage(){
+
+    const canvas = <any>document.getElementsByTagName('canvas')[0];
+
+    const b = canvas.toDataURL();
+    this.base64=b.substr(b.indexOf(',')+1);
+    console.log(this.base64);
+    this._service.submitImage(this.base64).subscribe(x=>{
+        console.log(x);
+      },err=>{console.log(err)});
+
+
+  /*  this.webcam.getBase64()
       .then( base=>{
         this.base64=base.substr(base.indexOf(',')+1);
         console.log(this.base64);
@@ -36,10 +63,14 @@ export class CaptureComponent implements OnInit {
             console.log(x);
           },err=>{console.log(err)});
       })
-      .catch( e=>console.error(e) )
+      .catch( e=>console.error(e) )*/
 
 
 
   };
+
+  discardImage(){
+    this.showCapture = false;
+  }
 
 }
