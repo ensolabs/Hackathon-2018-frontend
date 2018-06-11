3264b75259bae0fd6cda4a9cbef3b023d8b0cef9
Snorre Garmann <snorre@outlook.com>
2018-06-11 21:44:47 +0200

    added edit function

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -13,15 +13,15 @@ export class AdminComponent implements OnInit {
   public newTask: string;
   public tasks: Task[];
   ngOnInit() {
-    this._service.getTasks().subscribe(x => {this.tasks = x;  }, err => console.log(err));
+    this._service.getTasks().subscribe(x => { this.tasks = x; }, err => console.log(err));
   }
   post() {
     const p = this.newTask.split(',');
     const name = p[0];
-    const score = Number( p[p.length - 1] );
+    const score = Number(p[p.length - 1]);
     const keywords = p.slice(0, p.length - 1);
     const task = new Task('http://arngren.net', keywords, name, '1',
-      (Math.max(...(this.tasks.map(x => Number(x.RowKey)))) + 1).toString(), score );
+      (Math.max(...(this.tasks.map(x => Number(x.RowKey)))) + 1).toString(), score);
     this._service.newTask(task).subscribe(x => {
       this.tasks.push(task);
       this.newTask = '';
@@ -31,9 +31,16 @@ export class AdminComponent implements OnInit {
     return Array.prototype.join.call(task.Keywords, ',');
   }
   delete(task: Task) {
-    this._service.deleteTask(task.PartitionKey, task.RowKey).subscribe(x => {this.removeTask(task.RowKey); }, err => {console.log(err); });
+    this._service.deleteTask(task.PartitionKey, task.RowKey)
+      .subscribe(x => { this.removeTask(task.RowKey); }, err => { console.log(err); });
   }
   removeTask(rowKey: string) {
     this.tasks = this.tasks.filter(x => x.RowKey !== rowKey);
+  }
+  edit(task: Task) {
+
+    this.newTask = task.Name + ',' + task.Keywords.join(',') + ',' + task.Score;
+    this._service.deleteTask(task.PartitionKey, task.RowKey)
+    .subscribe(x => { this.removeTask(task.RowKey); }, err => { console.log(err); });
   }
 }
